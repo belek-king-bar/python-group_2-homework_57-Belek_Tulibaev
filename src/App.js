61f914b2d8377a4bc1136de563a376b60b7c3621
belek <belektolubaev@gmail.com>
2019-02-14 00:40:20 +0600

    App проекта

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Modal from './Components/UI/Modal/Modal.js';
+import ButtonRow from "./Components/UI/ButtonRow/ButtonRow";
+import Alert from './Components/UI/Alert/Alert.js';
 
 class App extends Component {
+
+  state = {
+    purchasable: true,
+    purchasing: true
+};
+
+
+  purchaseCancelHandler = () => {
+    this.setState({purchasing: false});
+  };
+
+  purchaseCloseHandler = () => {
+    this.setState({purchasable: false});
+  };
+
+  purchaseContinueHandler = () => {
+    alert('You continued!');
+  };
+
+
+
+
   render() {
+
+      const buttons = [
+            {type: 'success', label: 'Continue', clicked: this.purchaseContinueHandler},
+            {type: 'danger', label: 'Close', clicked: this.purchaseCancelHandler}
+        ];
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Modal show={this.state.purchasing} cancel={this.purchaseCancelHandler} title="Some kinda modal title" closed={this.purchaseCancelHandler}>
+                <p>This is modal content</p>
+                <ButtonRow buttons={buttons}/>
+          </Modal>
+          <Alert show={this.state.purchasable} type='warning' dismiss={this.purchaseCloseHandler}>
+              This is a warning type alert
+          </Alert>
+          <Alert show={this.state.purchasable} type='danger' dismiss={this.purchaseCloseHandler}>
+              This is a danger type alert
+          </Alert>
+
       </div>
     );
   }
